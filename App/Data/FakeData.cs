0e244c930d60125efd76559787a98d304fbc62ee
Timothy Emmanuel Kasasa <ekastimo@gmail.com>
2019-08-23 11:54:12 +0300

    Fixed data faker

--- App/Data/FakeData.cs
+++ App/Data/FakeData.cs
@@ -5,6 +5,7 @@ using App.Areas.Events.ViewModels;
 using Bogus;
 using System.Linq;
 using App.Areas.Chc.ViewModel;
+using App.Areas.Crm.Models;
 using App.Areas.Crm.ViewModels;
 
 namespace App.Data
@@ -21,7 +22,6 @@ namespace App.Data
 
         public static List<LocationViewModel> FakeLocations()
         {
-          
             return new List<LocationViewModel>
             {
                 new LocationViewModel
@@ -29,84 +29,83 @@ namespace App.Data
                     Id = "WHKatiKati",
                     Name = "WHKatiKati",
                     Details = "",
-                    MeetingTimes = new List<string>{ "9am", "11am" },
+                    MeetingTimes = new List<string> {"9am", "11am"},
                 },
                 new LocationViewModel
                 {
                     Id = "WHNalya",
                     Name = "WHNalya",
                     Details = "",
-                    MeetingTimes = new List<string>{ "9am", "11am" },
+                    MeetingTimes = new List<string> {"9am", "11am"},
                 },
                 new LocationViewModel
                 {
                     Id = "WHJinja",
                     Name = "WHJinja",
                     Details = "",
-                    MeetingTimes = new List<string>{ "9am", "11am" },
+                    MeetingTimes = new List<string> {"9am", "11am"},
                 },
                 new LocationViewModel
                 {
                     Id = "WHGayaza",
                     Name = "WHGayaza",
                     Details = "",
-                    MeetingTimes = new List<string>{ "9am", "11am" },
+                    MeetingTimes = new List<string> {"9am", "11am"},
                 },
                 new LocationViewModel
                 {
                     Id = "WHBugolobi",
                     Name = "WHBugolobi",
                     Details = "",
-                    MeetingTimes = new List<string>{ "9am", "11am" },
+                    MeetingTimes = new List<string> {"9am", "11am"},
                 },
                 new LocationViewModel
                 {
                     Id = "WHEntebe",
                     Name = "WHEntebe",
                     Details = "",
-                    MeetingTimes = new List<string>{ "9am", "11am" },
+                    MeetingTimes = new List<string> {"9am", "11am"},
                 },
                 new LocationViewModel
                 {
                     Id = "WHMukono",
                     Name = "WHMukono",
                     Details = "",
-                    MeetingTimes = new List<string>{ "9am", "11am" },
+                    MeetingTimes = new List<string> {"9am", "11am"},
                 },
                 new LocationViewModel
                 {
                     Id = "WHKubuye",
                     Name = "WHKubuye",
                     Details = "",
-                    MeetingTimes = new List<string>{ "9am", "11am" },
+                    MeetingTimes = new List<string> {"9am", "11am"},
                 },
                 new LocationViewModel
                 {
                     Id = "WHMakerere",
                     Name = "WHMakerere",
                     Details = "",
-                    MeetingTimes = new List<string>{ "10am" },
+                    MeetingTimes = new List<string> {"10am"},
                 },
                 new LocationViewModel
                 {
                     Id = "WHMbarara",
                     Name = "WHMbarara",
                     Details = "",
-                    MeetingTimes = new List<string>{"10am" },
+                    MeetingTimes = new List<string> {"10am"},
                 },
                 new LocationViewModel
                 {
                     Id = "WHDowntown",
                     Name = "WHDowntown",
                     Details = "",
-                    MeetingTimes = new List<string>{"10am" },
+                    MeetingTimes = new List<string> {"10am"},
                 }
             };
         }
 
         public static List<CellGroupViewModel> FakeCellGroups(int count = 100)
         {
-
             return new List<CellGroupViewModel>
             {
                 new CellGroupViewModel
@@ -115,7 +114,7 @@ namespace App.Data
                     Id = "DUNAMIS",
                     Name = "DUNAMIS",
                     Details = "DUNAMIS",
-                    MeetingTimes =  new List<string>{"6pm"},
+                    MeetingTimes = new List<string> {"6pm"},
                 },
                 new CellGroupViewModel
                 {
@@ -123,7 +122,7 @@ namespace App.Data
                     Id = "KMC",
                     Name = "KMC",
                     Details = "KMC",
-                    MeetingTimes =  new List<string>{"6pm"},
+                    MeetingTimes = new List<string> {"6pm"},
                 }
             };
         }
@@ -136,28 +135,29 @@ namespace App.Data
         public static List<ContactViewModel> FakeContacts(int count = 100)
         {
             var persons = new Faker<ContactViewModel>()
-                .RuleFor(u => u.MetaData.ChurchLocation, "WHKatiKati")
-                .RuleFor(u => u.MetaData.CellGroup, "DUNAMIS")
-
-
+                .RuleFor(u => u.MetaData, f => new MetaData
+                {
+                    ChurchLocation = "WHKatiKati",
+                    CellGroup = "DUNAMIS"
+                })
                 .RuleFor(u => u.Category, ContactCategory.Person)
-                .RuleFor(u => u.Person.FirstName, (f, u) => f.Name.FirstName())
-                .RuleFor(u => u.Person.LastName, (f, u) => f.Name.LastName())
-                .RuleFor(u => u.Person.MiddleName, (f, u) => f.Name.LastName())
-                .RuleFor(u => u.Person.Gender, f => f.PickRandom<Gender>())
-                .RuleFor(u => u.Person.CivilStatus, f => f.PickRandom<CivilStatus>())
-                .RuleFor(u => u.Person.Salutation, f => f.PickRandom<Salutation>())
-                .RuleFor(u => u.Person.About, f => f.Lorem.Paragraph())
-                .RuleFor(u => u.Person.Avatar, f => f.Internet.Avatar())
-
-
+                .RuleFor(u => u.Person, f => new PersonViewModel
+                {
+                    FirstName = f.Name.FirstName(),
+                    LastName = f.Name.LastName(),
+                    MiddleName = f.Name.LastName(),
+                    Gender = f.PickRandom<Gender>(),
+                    CivilStatus = f.PickRandom<CivilStatus>(),
+                    Salutation = f.PickRandom<Salutation>(),
+                    About = f.Lorem.Paragraph(),
+                    Avatar = f.Internet.Avatar()
+                })
                 .RuleFor(u => u.Events, f => ToArray(
                     new ContactEventViewModel
                     {
                         Category = ContactEventCategory.Birthday,
                         Value = f.Date.Past()
                     }))
-
                 .RuleFor(u => u.Emails, (f, u) => ToArray(
                     new EmailViewModel
                     {
@@ -171,7 +171,7 @@ namespace App.Data
                         Value = f.Phone.PhoneNumber("07########")
                     }))
                 .RuleFor(u => u.Tags, f => f.PickRandom(ContactTags.Select(it => it.ToLower()), 3).ToArray());
-                
+
             return persons.Generate(count);
         }
 
